9855e428fcd1fe1e6a9263fa29eb6038c7a44ace
xcm <853641294@qq.com>
2023-07-01 15:10:47 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div>11111</div>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+</script>
+<style lang="scss" scoped>
+.container {
+  color: red;
+  div {
+    background-color: black;
+  }
+}
+</style>
---
+++ src/components/A.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    i am componentA
+  </div>
+</template>
+<script setup lang='ts'>
+import { reactive,ref } from 'vue'
+
+</script>
+<style lang="scss" scoped>
+
+</style>
